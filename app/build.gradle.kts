a41bf21fabc8be14e3093c4bee4a0f8b308a3b33
z18836117021-cloud <z18836117021@gmail.com>
2026-08-20 21:56:23 +0800

    Add Android app build configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.deskpet"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.deskpet"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
